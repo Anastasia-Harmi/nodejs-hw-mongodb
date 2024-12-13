6daa552f20de18b16230eaa10ca06a9d9f57a1e0
Anastasia <tildatopdev@gmail.com>
2024-12-13 15:51:42 +0200

    add connection to db

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
-
+import { getEnvVar } from '../utils/getEnvVar.js';
+// const dbHost = getEnvVar("DB_HOST"); //коли всю ссилку винесли в
 export const initMongoDB = async () => {
   try {
+    const user = getEnvVar("MONGOBB_USER");
+    const password = getEnvVar("MONGOBB_PASSWORD");
+    const url = getEnvVar("MONGOBB_URL");
+    const db = getEnvVar("MONGOBB_DB");
     await mongoose.connect(
-      'mongodb+srv://Anastasia:L03IDfAcMV3rhBn3@cluster0.bkoow.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=Cluster0',
+      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`
     );
-    // const user =
-    // const password =
-    // const url =
-    // const db =
+    console.log('good');
+
   } catch (error) {
     console.log(`Error connection Mongo ${error.message}`);
     throw error;
