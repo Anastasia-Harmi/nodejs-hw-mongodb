9d12118a8b2711ee2e700ea839b707049ad069d8
Anastasia <tildatopdev@gmail.com>
2025-01-13 18:45:32 +0200

    fix

--- src/utils/calcPaginationData.js
+++ src/utils/calcPaginationData.js
@@ -1,12 +1,12 @@
-export const calcPaginationData = ({total, page, perPage}) => {
-const totalPages = Math.ceil(total/perPage); //ск всього стр;
+export const calcPaginationData = ({totalItems, page, perPage}) => {
+const totalPages = Math.ceil(totalItems/perPage); //ск всього стр;
 const hasNextPage = page < totalPages; // true-наступна сторінка є;
 const hasPrevPage = page > 1; // чи є попередня стр, можна записати і як page!==1
 
 return {
-    // page,
-    // perPage,
-    // total,
+    page,
+    perPage,
+    totalItems,
     totalPages,
     hasNextPage,
     hasPrevPage,
