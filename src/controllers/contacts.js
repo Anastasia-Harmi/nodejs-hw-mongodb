1b1f7bd983cd5964b584d18601e424fa2c07ed89
Anastasia <tildatopdev@gmail.com>
2025-01-23 16:38:34 +0200

    fix params

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -28,7 +28,7 @@ export const getContactsContrller = async (req, res) => {
 export const getContactsByIdContrller = async (req, res) => {
   console.log(req.params);
   const { _id: userId } = req.user;
-  const { contactId: _id } = req.params;
+  const { id: _id } = req.params;
   const data = await contactsServices.getContactById({ _id, userId });
 
   if (!data) {
